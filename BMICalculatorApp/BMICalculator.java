c98088148baf9b41cec62e9f762dbe1fdafdb3f6
AmirKhanzade <102859076+AmirKhanzade@users.noreply.github.com>
2025-01-22 21:16:45 +0100

    Add files via upload

---
+++ BMICalculatorApp/BMICalculator.java
@@ -0,0 +1,48 @@
+public class BMICalculator {
+
+    // Please declare 4 attributes/properties to represent weight in pounds, height in inches, weight in kilos and height in meters
+    double weightinPounds;
+    double heightinInches;
+    double weightinKilos;
+    double heightinMeters;
+
+
+
+    // Please use this default no arguments constructor to initialize the 4 properties to some initial values
+    public BMICalculator() {
+        this.weightinKilos=0;
+        this.weightinPounds=132;
+        this.heightinInches=6;
+        this.heightinMeters=1.78;
+
+
+    }
+
+    // Please implement the calculateBmiImperial() method to take in weight and height to calculate and return the BMI value in the Imperial system
+    public double calculateBmiImperial(double weight,double height) {
+        return (703*weight)/(height*height);
+
+    }
+
+    // Please implement the calculateBmiMetric() method to take in weight and height to calculate and return the BMI value in the Metric system
+    public double calculateBmiMetric(double height,double weigth) {
+        return weigth/(height*height);
+    }
+
+    // Please implement the getBMICategory() method so that it takes the BMI value and returns the BMI category based on it
+    public String getBMICategory(double bmi) {
+        if(bmi<18.5){
+            return "Underweigth";
+        }
+        if(bmi>=18.5&&bmi<25){
+            return "Normal weight";
+        }
+        if(bmi<=25&&bmi<30){
+            return "Overweight";
+        }
+        else{
+            return "Obese";
+        }
+
+    }
+}
